2a8eec05c2992349660e00af507404856bb4cd8e
joserosajunior <joserosajunior3@gmail.com>
2024-06-25 12:23:08 -0300

    estudo

---
+++ EstudoColections/src/OperacoesBasicas/Item.java
@@ -0,0 +1,24 @@
+package OperacoesBasicas;
+
+public class Item {
+
+	public Item(String nome, double preco, int quantidade) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNome() {
+		// TODO Auto-generated method stub
+		return ("nome");
+	}
+
+	public int getQuant() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	public int getPreco() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
